0952facd8ff261940fb3ebbda9ac83a20738669e
Talal Emran <49583599+talalemran@users.noreply.github.com>
2025-02-22 14:24:27 -0500

    Add files via upload

---
+++ exe01.py
@@ -0,0 +1,13 @@
+# This is my first Python Program
+print("Hello, World!")
+firstName = "Talal"
+lastName = "Emran"
+print(firstName + " " + lastName)
+print("I am a Python Programmer")
+# Get the first and last name and age from the user
+firstName = input("Enter your first name: ")
+lastName = input("Enter your last name: ")
+age = input("Enter your age: ")
+print("Hello, " + firstName + " " + lastName)
+print("You are " + age + " years old")
+print("wow, Python is amazing language!")
